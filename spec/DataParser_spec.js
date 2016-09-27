3aa661a03bed5d45ee274dbbe0df545b315a80f5
Gabriel Velasco <gabriel.velasco001@gmail.com>
2016-09-27 10:40:33 -0400

    Created the files, now they need to do something

---
+++ spec/DataParser_spec.js
@@ -0,0 +1,8 @@
+var DataParser = require('../src/DataParser');
+
+describe('Data Parser', function() {
+  it('should fail this test', function() {
+    DataParser();
+    expect(true).toBe(false);
+  });
+});
---
+++ spec/Translator_spec.js
@@ -0,0 +1,7 @@
+var Translator = require('../src/Translator')
+
+describe('Translator', function() {
+  it('should fail', function() {
+    expect(true).toBe(false);
+  });
+});
