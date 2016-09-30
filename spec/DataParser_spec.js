027f0b1b6cf6b952a63daaaf8641fedd20f2e68d
John Le <johnminhle01@gmail.com>
2016-09-30 01:37:00 -0400

    I DID IT

--- spec/DataParser_spec.js
+++ spec/DataParser_spec.js
@@ -1,9 +1,151 @@
 var DataParser = require('../src/DataParser');
 
 describe('Data Parser', function() {
-  it('should return empty', function() {
-    var dataParser = new DataParser("");
+  it('should return empty object for empty data', function() {
+    var raw_data = new DataParser("");
     var expected = { opal_diagnostics: {}, temperature_history: {} };
-    expect(dataParser.parse()).toEqual(expected);
+
+    expect(raw_data.parse()).toEqual(expected);
+  });
+
+  it('should return object with one opal log data entry', function() {
+    var raw_data = new DataParser("00,00,00,");
+    var expected = {
+      opal_diagnostics: {
+        1: "00,00,00,"
+      },
+       temperature_history: {}
+     };
+
+     expect(raw_data.parse()).toEqual(expected);
+  });
+
+  it('should return object with two opal log data entries', function() {
+    var raw_data = new DataParser("00,00,00,:::::A1,12,B2,");
+    var expected = {
+      opal_diagnostics: {
+        1: "00,00,00,",
+        2: "A1,12,B2,"
+      },
+       temperature_history: {}
+     };
+
+     expect(raw_data.parse()).toEqual(expected);
+  });
+
+  it('should return object with multiple opal log data entries', function() {
+    var raw_data = new DataParser("-- DATA DUMP (index: <04>) -- \
+    00,00,00,:::00,00,00,:::E0,0B,00,:::67,02,00,:::4C,10,00,:::C7,04,00,:::");
+    var expected = {
+      opal_diagnostics: {
+        1: "00,00,00,",
+        2: "00,00,00,",
+        3: "E0,0B,00,",
+        4: "67,02,00,",
+        5: "4C,10,00,",
+        6: "C7,04,00,",
+      },
+       temperature_history: {}
+     };
+
+     expect(raw_data.parse()).toEqual(expected);
+  });
+
+  it('should return object with one temperature history entry', function() {
+    var raw_data = new DataParser("Sep 7, 2016, 4:11 PM:14,");
+    var expected = {
+      opal_diagnostics: {
+      },
+      temperature_history: {
+        1: {
+          recorded: {
+            date: "Sep 7, 2016",
+            time: "4:11 PM",
+          },
+          temperature: 14
+        }
+      }
+     };
+
+     expect(raw_data.parse()).toEqual(expected);
+  });
+
+  it('should return object with two temperature history entries', function() {
+    var raw_data = new DataParser("Sep 7, 2016, 4:12 PM:44,Sep 7, 2016, 4:13 PM:46,");
+    var expected = {
+      opal_diagnostics: {
+      },
+      temperature_history: {
+        1: {
+          recorded: {
+            date: "Sep 7, 2016",
+            time: "4:12 PM",
+          },
+          temperature: 44
+        },
+        2: {
+          recorded: {
+            date: "Sep 7, 2016",
+            time: "4:13 PM",
+          },
+          temperature: 46
+        }
+      }
+     };
+
+     expect(raw_data.parse()).toEqual(expected);
+  });
+
+  it('should return object with multiple temperature history entries', function() {
+    var raw_data =
+      new DataParser(" TEMP DATA \
+        Sep 7, 2016, 4:11 PM:14, \
+        Sep 7, 2016, 4:12 PM:20, \
+        Sep 7, 2016, 4:13 PM:22, \
+        Sep 7, 2016, 4:14 PM:23, \
+        Sep 7, 2016, 4:15 PM:25,");
+    var expected = {
+      opal_diagnostics: {
+      },
+      temperature_history: {
+        1: {
+          recorded: {
+            date: "Sep 7, 2016",
+            time: "4:11 PM",
+          },
+          temperature: 14
+        },
+        2: {
+          recorded: {
+            date: "Sep 7, 2016",
+            time: "4:12 PM",
+          },
+          temperature: 20
+        },
+        3: {
+          recorded: {
+            date: "Sep 7, 2016",
+            time: "4:13 PM",
+          },
+          temperature: 22
+        },
+        4: {
+          recorded: {
+            date: "Sep 7, 2016",
+            time: "4:14 PM",
+          },
+          temperature: 23
+        },
+        5: {
+          recorded: {
+            date: "Sep 7, 2016",
+            time: "4:15 PM",
+          },
+          temperature: 25
+        },
+      }
+     };
+
+     expect(raw_data.parse()).toEqual(expected);
   });
 });
--- src/DataParser.js
+++ src/DataParser.js
@@ -1,18 +1,51 @@
-class DataParser {
-  constructor(raw_data) {
-    this.raw_data = raw_data;
-    console.log("LOL");
-  }
-
-  parse() {
-    return {
+module.exports = function(raw_data) {
+    var parsed_data = {
       opal_diagnostics: {},
       temperature_history: {}
     };
-  }
-}
 
-module.exports = DataParser;
+    var diagnostic_data_format =
+      /[A-Z0-9]{2},([A-Z0-9]{2},)+/g;
+    var temperature_format =
+      /[A-Za-z]{3}\s\d{1,2},\s\d{4},\s\d{1,2}:\d{1,2}\s[A-Z]{2}:\d{1,2}/g;
+
+  compileDiagnosticEntries = function(data) {
+    if(data.search(diagnostic_data_format) > -1)
+    {
+      var diagnostic_entries = data.match(diagnostic_data_format);
+
+      for(var log_index = 0; log_index < diagnostic_entries.length; log_index++)
+      {
+        parsed_data.opal_diagnostics[log_index + 1] = diagnostic_entries[log_index];
+      }
+    }
+  };
+
+  compileTemperatureHistory = function (data) {
+    if(data.search(temperature_format) > -1)
+    {
+      var temperature_entries = data.match(temperature_format);
+
+      for(var history_index = 0; history_index < temperature_entries.length; history_index++)
+      {
+        parsed_data.temperature_history[history_index + 1] =  {
+          recorded: {
+            date: temperature_entries[history_index].match(/[A-Za-z]{3}\s\d{1,2},\s\d{4}/g)[0],
+            time: temperature_entries[history_index].match(/\d{1,2}:\d{1,2}\s[A-Z]{2}/g)[0]
+          },
+          temperature: parseInt(temperature_entries[history_index].match(temperature_format)[0].slice(-2))
+        };
+      }
+    }
+  };
+
+  this.parse = function() {
+    compileDiagnosticEntries(raw_data);
+    compileTemperatureHistory(raw_data);
+
+    return parsed_data;
+  };
+};
 // {
 //   diagnostic_entries = {
 //     [0] = "00,00, ...",
