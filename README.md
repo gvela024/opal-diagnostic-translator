65fa6bf5f7aa52b2c50a3885f1ca19bf8f556fc5
Gabriel Velasco <gabriel.velasco001@gmail.com>
2016-10-02 09:54:00 -0400

    Added some more file error checking and some more edits tot he README

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ You must have `node.js` installed in your machine.
 2. `cd` into the root directory
 3. Run `npm install`
 4. Run the application with `node index.js [path/to/raw/data/file.txt]`. For an example, run `node index.js opal-data/data-set-1.txt`. The results will be printed to the console. If you would like to save them to a file, use `node index.js [path/to/raw/data/file.txt] >> name-of-file.json`.
+
+## Things to consider
+This has hardly any error checking. Unexpected behavior will result due to misuse.
--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var DataParser = require('./src/DataParser');
 var Translator = require('./src/Translator');
 
-// console.log(process.argv[2]);
-if(process.argv[2] === undefined) {
-  console.error('ERROR: Missing input file.');
+if (process.argv[2] === undefined) {
+  console.error('\nError: Missing input file.\n');
   process.exit(1);
 }
 
 var fs = require('fs');
 var filename = process.argv[2];
-var rawData = fs.readFileSync(filename, 'utf8');
+try {
+  var rawData = fs.readFileSync(filename, 'utf8');
 
-var translator = Translator();
-var dataParser = new DataParser(rawData);
+  var translator = Translator();
+  var dataParser = new DataParser(rawData);
 
-var dataObject = dataParser.parse();
-dataObject.diagnostics = translator.translateToObject(dataObject);
-console.log(JSON.stringify(dataObject, null, 3));
+  var dataObject = dataParser.parse();
+  dataObject.diagnostics = translator.translateToObject(dataObject);
+  console.log(JSON.stringify(dataObject, null, 3));
+} catch (error) {
+  console.error('\n' + error + '\n');
+}
