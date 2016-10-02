ed2bf21a506b1e88cb271360eb23db202f220380
Gabriel Velasco <gabriel.velasco001@gmail.com>
2016-10-02 07:47:11 -0400

    forcing a conflict

--- spec/Translator_spec.js
+++
@@ -1,7 +0,0 @@
-var Translator = require('../src/Translator');
-
-xdescribe('Translator', function() {
-    it('should fail', function(console, data_object) {
-    expect(true).toBe(false);
-  });
-});
--- src/Translator.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(print_function, data_object) {
-// Grab each chunk
-// Translate it into time
-// Add a descriptor
-// Append to whole string
-// Print the whole string
-};
